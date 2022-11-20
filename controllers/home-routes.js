967698a5cf17c338355f4f46e8237d691462b99f
Caitlin O'Reilly <coreilly29@gmail.com>
2022-11-20 15:27:31 -0500

    posts previously created are showing up on homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,10 +1,36 @@
+const { Post, Comment, User } = require("../models");
+
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-  res.render("homepage");
+  Post.findAll({
+    attributes: ["title", "body"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["body"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("homepage", { posts });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-// route to login 
+// route to login
 router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
     res.redirect("/");
