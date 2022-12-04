991527709c33977e3546b98b044c9f6c7417a12f
Caitlin O'Reilly <coreilly29@gmail.com>
2022-12-03 20:29:53 -0500

    working on comment functionality

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,7 +1,31 @@
-const router = require('express').Router();
-const withAuth = require('../../utils/auth.js');
-const { Comment } = require('../../models');
+const router = require("express").Router();
+const withAuth = require("../../utils/auth.js");
+const { Comment } = require("../../models");
 
+router.get("/", (req, res) => {
+  Comment.findAll()
+    .then((dbCommentData) => res.json(dbCommentData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
+// ability to post a comment when loggedIn
+router.post("/", withAuth, (req, res) => {
+  // check the session
+  if (req.session) {
+    Comment.create({
+      body: req.body.body,
+      post_id: req.body.post_id,
+      // use the id from the session
+      user_id: req.session.user_id,
+    }).then((dbCommentData) => res.json(dbCommentData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  }
+});
 
 module.exports = router;
